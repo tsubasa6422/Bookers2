a1f907990b45fb633a5947b4458253a050256fd1
Tsubasa Yamada <220030647+tsubasa6422@users.noreply.github.com>
2025-10-23 06:29:06 +0000

    サイドバー作成8割完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,21 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email])
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_up_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
+
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
 import "bootstrap";
 import "@fortawesome/fontawesome-free/css/all.css"; // ←これを使う！
 import "../stylesheets/application.scss";
+import Rails from "@rails/ujs";
+Rails.start();
+
